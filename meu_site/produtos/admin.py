d5040fc961cf6c68b2e4b824af4939b74177d9d2
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-04 09:32:28 -0300

    meu

--- meu_site/produtos/admin.py
+++ meu_site/produtos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Produto, Categoria
 
-# Register your models here.
+admin.site.register(Produto)
+admin.site.register(Categoria)
+
--- meu_site/produtos/models.py
+++ meu_site/produtos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    imagem = models.URLField()  # URL da imagem externa
+    link_compra = models.URLField()  # Link para a loja externa
+    categoria = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
